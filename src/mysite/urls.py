4d963751aa197383e7ba5d729667cbde06751ef9
Nils Witt <nils@nils-witt.de>
2025-08-04 17:41:16 +0200

    Enhance permission checks to ensure only authenticated users can access safe methods

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -34,8 +34,15 @@ class HasApiPermissions(permissions.BasePermission):
     Object-level permission to only allow owners of an object to edit it.
     Assumes the model instance has an `owner` attribute.
     """
-
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return False
     def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
         if request.user.is_superuser:
             return True
         else:
