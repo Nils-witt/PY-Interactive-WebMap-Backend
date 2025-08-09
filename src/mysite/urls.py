c63bae69cc52c6b197f87735f64903732ed604be
Nils Witt <nils@nils-witt.de>
2025-08-09 22:51:05 +0200

    Updated Permissions to access api

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -34,12 +34,14 @@ class HasApiPermissions(permissions.BasePermission):
     Object-level permission to only allow owners of an object to edit it.
     Assumes the model instance has an `owner` attribute.
     """
+
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
         if request.method in permissions.SAFE_METHODS:
             return True
-        return False
+        return request.user.is_superuser
+
     def has_object_permission(self, request, view, obj):
         if not request.user.is_authenticated:
             return False
