7dac8c12673ab3a6334d7b74d1164cc395b70b09
Nils Witt <nils.witt@nils-witt.de>
2025-12-24 23:48:31 +0100

    Added JWT Header Auth

--- src/mysite/asgi.py
+++ src/mysite/asgi.py
@@ -9,17 +9,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 from mysite.routing import websocket_urlpatterns
+from mysite.ws.JWTAuthStack import JWTAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": JWTAuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
         )
