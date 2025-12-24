c783d3afa0c561f57a8db33ab499ecdf587aad8e
Nils Witt <nils.witt@nils-witt.de>
2025-12-24 23:49:07 +0100

    Changed URL to /api/ws

--- src/mysite/routing.py
+++ src/mysite/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .consumers import MyConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/hello/$', MyConsumer.as_asgi()),
+    re_path(r'api/ws/$', MyConsumer.as_asgi()),
 ]
