163946357c15e976003a2a719aee33feab0f90bc
Nils Witt <nils.witt@nils-witt.de>
2025-12-26 18:35:54 +0100

    Implement WebSocket command handling and model update functionality

--- src/mysite/consumers.py
+++ src/mysite/consumers.py
@@ -10,7 +10,7 @@ from objects.serializers import NamedGeoReferencedItemSerializerWS, MapGroupSeri
 
 
 class MyConsumer(WebsocketConsumer):
-    def __init__(self, *args: Unused, **kwargs: Unused):
+    def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.user = None
 
@@ -40,7 +40,29 @@ class MyConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_discard)("chat", self.channel_name)
 
     def receive(self, text_data):
-        pass
+        try:
+            text_data_json = json.loads(text_data)
+            command = text_data_json.get('command', '')
+            if command.lower() == 'ping':
+                self.send(text_data=json.dumps({
+                    'message': 'pong',
+                    'sender': 'system'
+                }))
+            if command.lower() == 'whoami':
+                self.send(text_data=json.dumps({
+                    'message': f'You are {self.user.get_username()}',
+                    'sender': 'system'
+                }))
+            if command.lower() == 'model.update':
+                self.model_update_received(text_data_json.get('model', None), text_data_json.get('id', None), text_data_json.get('data', None))
+        except Exception as e:
+            print(e)
+            self.send(text_data=json.dumps({
+                'message': 'Error processing message',
+                'sender': 'system',
+                'payload': text_data
+            }))
+
 
     def model_update(self, event):
         model = event["model_type"]
@@ -71,5 +93,38 @@ class MyConsumer(WebsocketConsumer):
                 'event': 'model.delete',
                 'model_type': model.__name__,
                 'object_id': str(event['object_id']),
+                'sender': 'system'
+            }))
+
+    def model_update_received(self, model_name: str, object_id: str, data):
+        self.send(text_data=json.dumps({
+            'event': 'model.update.received',
+            'model_type': model_name,
+            'object_id': object_id,
+            'data': data,
+            'sender': 'system'
+        }))
+
+        if model_name == "NamedGeoReferencedItem":
+            instance = NamedGeoReferencedItem.objects.get(id=object_id)
+            if 'name' in data.keys():
+                instance.name = data['name']
+            if 'latitude' in data.keys():
+                instance.latitude = data['latitude']
+            if 'longitude' in data.keys():
+                instance.longitude = data['longitude']
+            if 'zoom_level' in data.keys():
+                instance.zoom_level = data['zoom_level']
+            if 'show_on_map' in data.keys():
+                instance.show_on_map = data['show_on_map']
+            if 'description' in data.keys():
+                instance.description = data['description']
+            instance.save()
+        else:
+            self.send(text_data=json.dumps({
+                'event': 'model.update.error',
+                'model_type': model_name,
+                'object_id': object_id,
+                'message': 'Unknown or unsupported model type',
                 'sender': 'system'
             }))
